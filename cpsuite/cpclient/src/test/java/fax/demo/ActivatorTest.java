a279f232f95d66665119fb027e86e7feb8fba56f
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2021-09-15 14:45:12 +0200

    Try IncludeJars from both jar and test-jar

---
+++ cpsuite/cpclient/src/test/java/fax/demo/ActivatorTest.java
@@ -0,0 +1,15 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package fax.demo;
+
+import org.junit.extensions.cpsuite.ClasspathSuite;
+import org.junit.runner.RunWith;
+
+@RunWith(ClasspathSuite.class)
+@ClasspathSuite.IncludeJars(true)
+public class ActivatorTest {
+}
